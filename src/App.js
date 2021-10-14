a79544424128b972ef398103067a9bf20ee1654b
unknown <charleschow1999@gmail.com>
2021-10-14 14:20:09 -0400

    Prod test console log inside box

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 import { Component } from 'react';
 
 function App() {
+  console.log("appstarted")
   return (
       <Box>
         {/* <Main/> */}
+        test
       </Box>
   );
 }
